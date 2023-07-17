e24a89752b5b54df1478f7bd993eb4064d6a9fa7
tianze0926 <i@tianze.me>
2023-07-17 15:08:37 +0100

    fix(nvim-base): groupmod argument typo

--- nvim/base/init.fish
+++ nvim/base/init.fish
@@ -1,6 +1,6 @@
 if set -q PUID && set -q PGID
     usermod --non-unique --uid $PUID abc
-    groupmod --non-unique --uid $PGID abc
+    groupmod --non-unique --gid $PGID abc
 
     chown --quiet -R $PUID:$PGID /home/abc
 end
